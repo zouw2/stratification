82d6c274395d8597b3193a315703c1e13f66bc2d
zouw2 <zou_w@yahoo.com>
2023-12-19 10:16:28 -0800

    add functions to merge small strata

--- strat_functions.r
+++ strat_functions.r
@@ -119,6 +119,80 @@ check_para <- function(p) {
 }
 
 
+collapse_grp <- function(dsIn, minEvent = 10, rule = c('collapseGroup','removeStratum') ) {
+  stopifnot(all(c('grp','event') %in% colnames(dsIn)))
+  stopifnot(length(rule) == 1)
+    e1 <- with(dsIn, tapply(event,grp, sum))
+    if(any(e1) < minEvent){
+    if(rule == 'collapseGroup'){
+      group2Collapse <- names(e1)[e1 < minEvent]
+      if(sum(e1[group2Collapse]) >= minEvent) {
+        dsIn[dsIn$grp %in% group2Collapse,'grp'] <- 'MCollapse'
+      }else{
+        e2 <- e1[setdiff(names(e1), group2Collapse)]
+        nextSmallest <- names(e2)[which.min(e2)]
+        dsIn[dsIn$grp %in% group2Collapse,'grp'] <- nextSmallest
+      }
+    }
+      
+    if(rule == 'removeStratum') { # a very specific rule to remove the 2nd group; not general at all
+      dsIn[dsIn$grp %in% c('M1','M3'),'grp'] <- c('M13') 
+      dsIn[dsIn$grp %in% c('M2','M4'),'grp'] <- c('M24')
+      dsIn[dsIn$grp %in% c('M5','M7'),'grp'] <- c('M57') 
+      dsIn[dsIn$grp %in% c('M6','M8'),'grp'] <- c('M68')
+    }
+  }
+    
+    dsIn
+  }
+
+
+sim4 <- function( p  , time , maxPms = 10, rampUpTime, estimatePrognostic=F ) {
+# sim4 is similar to sim3, but it adds rules to collapse stratification groups: if the number of event is fewer than minE, the group will be collapsed
+  
+  #ai0 <- with(p, rep(1:maxPms, each=enrollmentSpeadFactor) * dup )
+  if(missing(rampUpTime)) rampUpTime <- round( time /5 )
+  ai0 <- with(p, seq(1, maxPms, length.out = rampUpTime) * dup )
+  ds1 <- do.call(rbind, lapply(1:length(p$n), function(i) {
+    	ds1a <-  sim_rpact2(n_total = p$n[i] * p$dup ,    ai=ai0 * p$n[i]/sum(p$n),   readoutTime=time, m_b = p$med[i], hr = p$hr[i], group_text=paste('M',i, sep='') )
+    	
+    		if( is.null(ds1a)  ) return(rep(NA, 11))
+    	  ds1a$dup <- NA
+    	  for (a in unique(ds1a$treatmentGroup)) {
+		        ds1a[ds1a$treatmentGroup == a,'dup'] <- sample( rep(1:p$dup, p$n[i]/2) )
+		
+	      } # enforce the stratified randomization
+    	  
+    	  ds1a
+    }) )
+   
+  
+	stopifnot(nrow(ds1) == with(p, sum(n) *dup))
+	c1 <- with(ds1, table(treatmentGroup,  dup, grp, useNA = 'ifany'))
+	for (i in 1:length(p$n)) {
+   stopifnot(all(as.vector(c1[,, i]) == p$n[i] /2  ))
+	}
+	 
+	res0 <- t ( sapply(split(ds1, paste(ds1$dup)), ana1, form= 'Surv(tte, event) ~ arm') )
+	res2 <-  t ( sapply(split(ds1, paste(ds1$dup)), function(d) {
+	  d2 <- collapse_grp(d,minEvent=p$minE, rule=p$collapseRule)
+ 
+	  c( ana1( d = d2 , form= 'Surv(tte, event) ~ arm + strata(grp)'), n_strata = length(unique(d2$grp)))
+	  }))
+	res_s <- t ( sapply(split(ds1, paste(ds1$dup)), ana1, form='Surv(tte, event) ~ arm + strata(grp)') )
+	
+	colnames(res2) <- paste(colnames(res2),'c', sep='_')
+	colnames(res_s) <- paste(colnames(res_s), 's', sep = '_')
+	#with(ds1, tapply(tte, list(arm, grp), median))
+	
+ stopifnot(all(row.names(res0) == row.names(res_s)))
+ 
+	cbind(res0[, c('n','nevent','p','qad_hr')], res_s[, c('p_s','qad_hr_s')], res2[, c('p_c','qad_hr_c','n_strata_c')], delta_p = res0[,'p'] - res_s[,'p_s'], delta_p2 = res2[,'p_c'] - res_s[,'p_s'] )
+
+}
+
+
+
 sim3 <- function( p  , time , maxPms = 10, rampUpTime, estimatePrognostic=F) {
     # a larger enrollmentSpeadFactor means slower wrap up
   #enrollmentSpeadFactor = 2
